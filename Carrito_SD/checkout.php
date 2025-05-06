ccf4c29405611f02a66fe9ce061937f0eb9c5934
Gael Alexander Carrillo Chan <al068322@uacam.mx>
2025-05-05 19:23:35 -0600

    modified:   Carrito_SD/checkout.php modified:   Carrito_SD/index.php

--- Carrito_SD/checkout.php
+++ Carrito_SD/checkout.php
@@ -8,7 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 
-
     <script src="https://www.paypal.com/sdk/js?client-id=AfhPKqCWEVgoiqgJR_Uun9b0mYQsu05qKt8q_-SuXg0zjqPfk8Jr5CXYExmQcvc5El18Enw1VkkGcYC9&currency=MXN"></script>
 </head>
 
